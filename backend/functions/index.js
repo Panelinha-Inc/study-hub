2c637482af333e512a061c9eed7bcb54c3820066
Hélio Rocha Vieira de Couto Junior <junior0helio15@gmail.com>
2020-09-16 23:01:34 -0300

    Get user data by uid
    Por conta de uma dificuldade encontrada, o objetivo desta tarefa teve de ser alterado, pois descobrimos que fazer login não é uma responsabilidade do backend, mas sim unicamente do app mobile em comunicação direta com o firebase auth. Portanto, nesta tarefa apenas desenvolvemos uma função que retornará o restante dos dados do usuário.
    
    Co-Authored-By: Açucena Rodrigues <acucenarodrigues1998@gmail.com>
    Co-Authored-By: Jederson Sousa Luz <jedersonalpha@gmail.com>
    Co-Authored-By: Vitória de Carvalho Brito  <33909398+VitoriaCarvalho@users.noreply.github.com>

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,11 @@
 const admin = require("firebase-admin");
 const functions = require('firebase-functions');
 
+const firebase = require('firebase')
+
+
 admin.initializeApp(functions.config().firebase);
+firebase.initializeApp(admin.credential)
 
 const auth = admin.auth();
 const usersDB = admin.firestore().collection('Users');
@@ -32,3 +36,17 @@ exports.createUser = functions.https.onRequest(async (req, res) => {
 
     res.json(user.uid)
 });
+
+exports.getUserData = functions.https.onRequest(async (req, res) => {
+    const { uid } = req.body;
+
+    res.json((await usersDB.doc(uid).get()).data());
+})
+
+// exports.login = functions.https.onRequest(async (req, res) => {
+//     const { email, password } = req.body;
+//     auth.getUserByEmail
+//     res.json(firebase.auth().signInWithEmailAndPassword(email, password));
+// })
+
+
