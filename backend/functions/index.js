540805be86d3b24a713bc515c03da1c11a0e7a3d
Jederson Sousa Luz <jedersonalpha@gmail.com>
2020-09-16 21:43:33 -0300

    Só comecei a implementar. '-'

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -6,6 +6,11 @@ admin.initializeApp(functions.config().firebase);
 const auth = admin.auth();
 const usersDB = admin.firestore().collection('Users');
 
+// Create a root reference
+const storageRef = firebase.storage().ref();
+
+const stream = require('stream');
+
 // const auth = require('./src/auth.js');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,18 +21,19 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.auth = functions.https.onRequest(async (req, res) => {
-    const { email, password, username, bio, locate, photoUrl, areasDeInteresse } = req.body;
+    const { email, password, username, bio, locate, photoBase64, areasDeInteresse } = req.body;
 
     const user = await auth.createUser({
         email,
         password,
         username,
     });
+    
+    storageRef.bucket('gs://studyhub-9f612.appspot.com/userPhotos')
 
     usersDB.doc(user.uid).set({
         bio,
         locate,
-        photoUrl,
         areasDeInteresse
     })
 
